3289481bb3f75767a69df4033be0f9c1fea6c09d
JuanVasquez2025 <vl25001@esfe.agape.edu.sv>
2025-03-24 10:04:19 -0600

    Se creo las vistas y controlador de Productos.

---
+++ JCVL20250324.AppWebMVC/Controllers/ProductController.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JCVL20250324.AppWebMVC.Models;
+
+namespace JCVL20250324.AppWebMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly Test20250324DbContext _context;
+
+        public ProductController(Test20250324DbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index(Product producto, int top = 10)
+        {
+            var query = _context.Products.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(producto.ProductName))
+                query = query.Where(s => s.ProductName.Contains(producto.ProductName));
+
+            if (producto.Id > 0)
+                query = query.Where(s => s.Id == producto.Id);
+
+            if (producto.WarehouseId > 0)
+                query = query.Where(s => s.WarehouseId == producto.WarehouseId);
+
+            query = query
+                            .Include(p => p.Warehouse)
+                            .Include(p => p.Brand)
+                            .Take(top);
+
+            var marcas = _context.Brands.ToList();
+            marcas.Add(new Brand { BrandName = "SELECCIONAR", Id = 0 });
+
+            var bodegas = _context.Warehouses.ToList();
+            bodegas.Add(new Warehouse { WarehouseName = "SELECCIONAR", Id = 0 });
+
+            ViewData["WarehouseId"] = new SelectList(bodegas, "Id", "WarehouseName", 0);
+            ViewData["BrandId"] = new SelectList(marcas, "Id", "BrandName", 0);
+
+            return View(await query.ToListAsync());
+        }
+
+        // GET: Product/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Warehouse)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // GET: Product/Create
+        public IActionResult Create()
+        {
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "BrandName");
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "WarehouseName");
+            return View();
+        }
+
+        // POST: Product/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,ProductName,Description,Price,PurchasePrice,WarehouseId,BrandId,Notes")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "BrandName", product.BrandId);
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Notes", product.WarehouseId);
+            return View(product);
+        }
+
+        // GET: Product/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "BrandName", product.BrandId);
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Notes", product.WarehouseId);
+            return View(product);
+        }
+
+      
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ProductName,Description,Price,PurchasePrice,WarehouseId,BrandId,Notes")] Product product)
+        {
+            if (id != product.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BrandId"] = new SelectList(_context.Brands, "Id", "BrandName", product.BrandId);
+            ViewData["WarehouseId"] = new SelectList(_context.Warehouses, "Id", "Notes", product.WarehouseId);
+            return View(product);
+        }
+
+        // GET: Product/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products
+                .Include(p => p.Brand)
+                .Include(p => p.Warehouse)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.Id == id);
+        }
+    }
+}
--- JCVL20250324.AppWebMVC/Models/Product.cs
+++ JCVL20250324.AppWebMVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace JCVL20250324.AppWebMVC.Models;
 
@@ -7,16 +8,30 @@ public partial class Product
 {
     public int Id { get; set; }
 
+
+
+    [Display(Name = "Nombre Del Producto")]
+    [Required(ErrorMessage = "Informacion Obligatorio")]
     public string ProductName { get; set; } = null!;
 
+    [Display(Name = "Descripcion")]
+    [Required(ErrorMessage = "Informacion Obligatorio")]
     public string? Description { get; set; }
 
+    [Display(Name = "Precio")]
+    [Required(ErrorMessage = "Precio Obligatorio")]
     public decimal Price { get; set; }
 
+    [Display(Name = "Precio de Compra")]
     public decimal PurchasePrice { get; set; }
 
+
+    [Display(Name = "Bodega")]
+    [Required(ErrorMessage = "Informacion Obligatorio")]
     public int? WarehouseId { get; set; }
 
+    [Display(Name = "Marca")]
+    [Required(ErrorMessage = "Obligatorio seleccionar marca")]
     public int? BrandId { get; set; }
 
     public string? Notes { get; set; }
