82812299dafbb74db9363330d04a9f2a79ddd878
JuanVasquez2025 <vl25001@esfe.agape.edu.sv>
2025-03-24 07:52:33 -0600

    Se realizo el scaffold.

---
+++ JCVL20250324.AppWebMVC/Models/Test20250324DbContext.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace JCVL20250324.AppWebMVC.Models;
+
+public partial class Test20250324DbContext : DbContext
+{
+    public Test20250324DbContext()
+    {
+    }
+
+    public Test20250324DbContext(DbContextOptions<Test20250324DbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<Warehouse> Warehouses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Brands__3214EC07A59B8995");
+
+            entity.Property(e => e.BrandName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Country)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Products__3214EC074A34DF27");
+
+            entity.Property(e => e.BrandId).HasColumnName("BrandID");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.Price).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.PurchasePrice).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.WarehouseId).HasColumnName("WarehouseID");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Products)
+                .HasForeignKey(d => d.BrandId)
+                .HasConstraintName("FK__Products__BrandI__4E88ABD4");
+
+            entity.HasOne(d => d.Warehouse).WithMany(p => p.Products)
+                .HasForeignKey(d => d.WarehouseId)
+                .HasConstraintName("FK__Products__Wareho__4D94879B");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Users__3214EC073477A93E");
+
+            entity.HasIndex(e => e.Email, "UQ__Users__A9D10534FC1B1EE3").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.Password)
+                .HasMaxLength(32)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.Role)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.Username)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Warehouse>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Warehous__3214EC0765C9ED51");
+
+            entity.Property(e => e.Notes).HasColumnType("text");
+            entity.Property(e => e.WarehouseName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
