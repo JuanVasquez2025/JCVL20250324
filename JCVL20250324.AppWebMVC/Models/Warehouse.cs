cfa50031e15e844166b4211fde945a88ae56b0e4
JuanVasquez2025 <vl25001@esfe.agape.edu.sv>
2025-03-24 08:05:59 -0600

    Se realizo el scaffold y se creo el controlador y vistas del modelo Warehouse.

--- JCVL20250324.AppWebMVC/Models/Warehouse.cs
+++ JCVL20250324.AppWebMVC/Models/Warehouse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace JCVL20250324.AppWebMVC.Models;
 
@@ -7,8 +8,14 @@ public partial class Warehouse
 {
     public int Id { get; set; }
 
+    [Required(ErrorMessage = "El nombre de la bodega es obligatorio")]
+    [StringLength(100, ErrorMessage = "El nombre de la bodega no puede exceder los 100 caracteres")]
+    [Display(Name = "Bodegas")]
     public string WarehouseName { get; set; } = null!;
 
+    [StringLength(500, ErrorMessage = "Las notas es demasiado larga")]
+    [Required(ErrorMessage = "La notas de la bodega es obligatorio")]
+    [Display(Name = "Notas")]
     public string? Notes { get; set; }
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
--- JCVL20250324.AppWebMVC/Program.cs
+++ JCVL20250324.AppWebMVC/Program.cs
@@ -1,9 +1,27 @@
+using JCVL20250324.AppWebMVC.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Test20250324DbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Conn"));
+});
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie((o) =>
+{
+    o.LoginPath = new PathString("/User/login");
+    o.AccessDeniedPath = new PathString("/User/login");
+    o.ExpireTimeSpan = TimeSpan.FromHours(8);
+    o.SlidingExpiration = true;
+    o.Cookie.HttpOnly = true;
+});
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
